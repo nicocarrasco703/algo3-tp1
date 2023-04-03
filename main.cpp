6d31c593122a9f85a8fb42ff62e7721b5622fc13
tmoraut07 <tobiasmoraut7@gmail.com>
2023-04-02 21:06:35 -0300

    primer ejercicio casi terminado

--- main.cpp
+++ main.cpp
@@ -2,64 +2,117 @@
 #include <vector>
 using namespace std;
 
-/*cuadrado(i,j){
-    if(i == n){
-        ifEsMagico(m) soluciones ++
-    }
-    else{
-        if(j+1 < n){
-            prox_i = i;
-            prox_j = j + 1
-        } else {
-            prox_i = i + 1;
-            prox_j = 0;
-        }
-        if(m[i][j] == 0){                      //else m[prox_i][prox_j]
-            for(int k = 1; k <= n^2; k++){
-                if(v[k] == 0){                 //v es un vector hasta n de 0s o 1s que me dicen si ya use el numero en cuestion.
-                m[i][j] = k;
-                v[k] = 1;
-                cuadrado(prox_i, prox_j);
-        }
-    }
-}*/
 int n;
-vector<int> v(n^2, 0);
-vector<vector<int>> m(n, vector<int>(n, 0));
+int k;
+int numeroMagico;
+vector<int> v;
+vector<vector<int>> m;
+int cont = 0;
 
-vector<vector<int>> construirCuadrado(int i, int j) {
-    if (j + 1 < n) {
-        int prox_i = i;
-        int prox_j = j + 1;
-    } else {
-        int prox_i = i + 1;
-        int prox_j = 0;
+bool filaSuperaNumero(int fila , int sum){
+    bool res;
+    int suma = 0;
+    for(int j = 0; j < n; j++){
+        suma = suma + m[fila][j];
+    }
+    res = suma > numeroMagico;
+    return res;
+}
 
-        if (m[i][j] == 0) {
-            for (int k = 1; k <= n ^ 2; k++) {
-                if (v[k] == 0) {
-                    m[i][j] = k;
-                    v[k] = 1;
-                    construirCuadrado(prox_i, prox_j);
-                }
-                return m;
+bool columnaSuperaNumero(int col, int sum){
+    bool res;
+    int suma = 0;
+    for(int i = 0; i < n; i++){
+        suma = suma + m[i][col];
+    }
+    res = suma > numeroMagico;
+    return res;
+}
+
+bool esMagico() {
+    int row_sum, col_sum, diag_sum1, diag_sum2;
+    for (int i = 0; i < n; i++) {
+        row_sum = col_sum = 0;
+        for (int j = 0; j < n; j++) {
+            row_sum += m[i][j];
+            col_sum += m[j][i];
+            if (filaSuperaNumero(i, numeroMagico) || columnaSuperaNumero(i, numeroMagico)) {
+                return false; // poda si la suma excede la suma mágica
+            }
+        }
+        if (row_sum != numeroMagico || col_sum != numeroMagico) {
+            return false;
+        }
+    }
+    diag_sum1 = diag_sum2 = 0;
+    for (int i = 0; i < n; i++) {
+        diag_sum1 += m[i][i];
+        diag_sum2 += m[i][n-i-1];
+    }
+    if (diag_sum1 != numeroMagico || diag_sum2 != numeroMagico) {
+        return false;
+    }
+    return true;
+}
+
+
+
+bool construirCuadrado(int i, int j){
+    int prox_i;
+    int prox_j;
+    if(i == n){
+        if(esMagico()) {
+            cont++;
+            if(cont == k){
+                return true;
             }
         }
     }
+    else {
+        if(j + 1 < n){
+            prox_i = i;
+            prox_j = j + 1;
+        }
+        else {
+            prox_i = i + 1;
+            prox_j = 0;
+        }
+        if (m[i][j] == 0) {
+            for (int x = 1;x <= n*n; x++) {
+                if (v[x - 1] == 0) {                        //v es un vector de 0 a n^2 que me dice si el elemento x esta usado o no en la matriz
+                    m[i][j] = x;
+                    v[x - 1] = 1;
+                    //if(!filaSuperaNumero(i , numeroMagico) && !columnaSuperaNumero(j, numeroMagico)){
+                        //construirCuadrado(prox_i, prox_j);
+                    //}
+                    if(construirCuadrado(prox_i, prox_j)){
+                        return true;
+                    }
+                    v[x - 1] = 0;
+                    construirCuadrado(i, j);
+                }
+            }
+        } else construirCuadrado(prox_i, prox_j);
+    }
 }
 
-
-bool esMagico(vector<vector<int>> res){
-
-}
 
 int main(){
-    n = 2;
-    vector<vector<int>> rta = construirCuadrado(0, 0);
-    for(int i = 0; i < n; i ++) {
-        for(int j = 0; j < n; j++){
-            cout << rta[i][j] << endl;
+    cin >> n;
+    cin >> k;
+    numeroMagico = ((n * n * n +n)/2);
+    m = vector(n, vector<int>(n, 0));
+    v = vector(n*n, 0);
+    if (construirCuadrado(0, 0)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << m[i][j] << " ";
+            }
+            cout << "\n";
         }
+    }
+    else{
+        cout << -1 << endl;
     }
     return 0;
 }

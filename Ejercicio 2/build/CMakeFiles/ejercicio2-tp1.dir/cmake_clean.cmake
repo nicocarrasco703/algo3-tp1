66c33067a74f2e135566bd812e71a1e8b05155bc
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-06 15:58:59 -0300

    Creados directorios para ej2

---
+++ Ejercicio 2/build/CMakeFiles/ejercicio2-tp1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio2-tp1.dir/main.cpp.o"
+  "CMakeFiles/ejercicio2-tp1.dir/main.cpp.o.d"
+  "ejercicio2-tp1"
+  "ejercicio2-tp1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio2-tp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ejercicio 2/build/CMakeFiles/ejercicio2-tp1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ejercicio2-tp1.
+# This may be replaced when dependencies are built.

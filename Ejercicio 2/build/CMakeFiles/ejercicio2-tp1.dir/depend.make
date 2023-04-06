1099ca62b0c969703cfa2bc3ae35e7b2c125bd2f
Nicolas Carrasco <104697490+nicocarrasco703@users.noreply.github.com>
2023-04-06 16:13:19 -0300

    Delete Ejercicio 2/build directory

--- Ejercicio 2/build/CMakeFiles/ejercicio2-tp1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ejercicio2-tp1.
-# This may be replaced when dependencies are built.
--- Ejercicio 2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nicolascarrasco/Desktop/AyED III/TPs/algo3-tp1/Ejercicio 2
-# Build directory: /Users/nicolascarrasco/Desktop/AyED III/TPs/algo3-tp1/Ejercicio 2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

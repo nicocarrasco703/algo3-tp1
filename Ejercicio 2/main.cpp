f6a3598d63bfb2895870bc1f56e9971e17eb72d1
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-09 21:53:00 -0300

    Primer aproach ej2

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+int n; int m; int r;
+vector<int> v;
+map<pair<int, int>, int> dynamic_map;
+
+bool busqueda(int i, int acum){
+    if (i == n) {
+        return acum % m == r;
+    }
+    if (dynamic_map.find({i, acum}) != dynamic_map.end()) {
+        return dynamic_map[{i, acum}];
+    }
+
+    bool resta = busqueda(i + 1, acum - v[i]);
+    bool suma = busqueda(i + 1, acum + v[i]);
+    bool producto = busqueda(i + 1, acum * v[i]);
+    bool potencia = busqueda(i + 1, pow (acum, v[i]));
+
+    return resta || suma || producto || potencia;
+}
+
+int main() {
+    cin >> n >> m >> r;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+    return 0;
 }

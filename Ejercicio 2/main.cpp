da074af2eaffa7250d80a4536b1ade028f7b1e30
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-10 21:55:21 -0300

    Ej 2 comentado

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -13,23 +13,25 @@ vector<string> resultados;
 
 
 bool busqueda(int i, int acum, map<pair<int, int>, bool> &dynamic_map){
-    pair<int, int> clave = make_pair(i, acum);
+    pair<int, int> clave = make_pair(i, acum); // creo la clave de antemano para no tener que hacerlo en cada llamada recursiva
 
-    if (dynamic_map.find(clave) != dynamic_map.end()) {
+    if (dynamic_map.find(clave) != dynamic_map.end()) { // si ya esta definido el valor de la clave, lo devuelvo
         return dynamic_map[clave];
     }
 
-    if (i == n) {
+    if (i == n) { // si llegue al final del vector guardo el valor en el diccionario y devuelvo el resultado
         bool res = (acum % m) == r;
         dynamic_map.emplace(clave, res);
         return res;
     }
 
+    // recursion
     bool resta = busqueda(i + 1, acum - v[i], dynamic_map);
     bool suma = busqueda(i + 1, acum + v[i], dynamic_map);
     bool producto = busqueda(i + 1, acum * v[i], dynamic_map);
     bool potencia = busqueda(i + 1, pow (acum, v[i]), dynamic_map);
 
+    // guardo el resultado en el diccionario y devuelvo el resultado
     dynamic_map.emplace(clave, resta || suma || producto || potencia);
     return dynamic_map[clave];
 }

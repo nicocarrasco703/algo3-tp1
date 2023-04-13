c7783d44c94a283628933956c0c5f98782ec7004
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-13 11:45:36 -0300

    Cambios Ej 2 sigue sin funcionar

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -5,14 +5,27 @@
 
 using namespace std;
 
-// declaracion de variables globales
-int n; int m; int r; int c;
+// declaracion de variables globales ((a % b) +b )%b
+int n; int m; unsigned long int r; int c;
 vector<int> v;
 vector<string> resultados;
 // -------------------------------
 
+unsigned long int power(int x, unsigned int y){
+     unsigned long int temp;
+     if( y == 0)
+       return 1;
+     if(y ==1)
+        return x % m;
+     temp = power(x, y/2);
+     if (y%2 == 0)
+       return (temp*temp) % m;
+     else
+       return (x*temp*temp) % m;
+  }
 
-bool busqueda(int i, int acum, map<pair<int, int>, bool> &dynamic_map){
+
+bool busqueda(int i, unsigned long int acum, map<pair<int, int>, bool> &dynamic_map){
     pair<int, int> clave = make_pair(i, acum); // creo la clave de antemano para no tener que hacerlo en cada llamada recursiva
 
     if (dynamic_map.find(clave) != dynamic_map.end()) { // si ya esta definido el valor de la clave, lo devuelvo
@@ -25,14 +38,14 @@ bool busqueda(int i, int acum, map<pair<int, int>, bool> &dynamic_map){
         return res;
     }
 
+    unsigned long int restoNeg = ((acum -v[i] % m) + m) % m;
     // recursion
-    bool resta = busqueda(i + 1, acum - v[i], dynamic_map);
-    bool suma = busqueda(i + 1, acum + v[i], dynamic_map);
-    bool producto = busqueda(i + 1, acum * v[i], dynamic_map);
-    bool potencia = busqueda(i + 1, pow (acum, v[i]), dynamic_map);
-
     // guardo el resultado en el diccionario y devuelvo el resultado
-    dynamic_map.emplace(clave, resta || suma || producto || potencia);
+    dynamic_map.emplace(clave, busqueda(i + 1, restoNeg, dynamic_map) ||
+     busqueda(i + 1, acum + v[i], dynamic_map) ||
+      busqueda(i + 1, acum * v[i], dynamic_map) || 
+      busqueda(i + 1, power(acum, v[i]), dynamic_map));
+
     return dynamic_map[clave];
 }
 

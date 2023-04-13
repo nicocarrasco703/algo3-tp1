523e61afc9b2f930ac5a6ad429fdbd63b81e8289
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-13 11:38:50 -0300

    Ej 1 terminado

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+
 using namespace std;
 
 int n;
@@ -9,27 +9,12 @@ int numeroMagico;
 vector<int> usados;
 vector<vector<int>> m;
 int cont = 0;
-int sumaParcialFila;
 vector<int> fil;
 vector<int> col;
-int sumaFila;
-int sumaColumna;
 int sumaDiagonal1;
 int sumaDiagonal2;
 
-
-
-
 bool esMagico() {
-    for (int i = 0; i < n; i++) {
-        sumaFila = 0;
-        sumaColumna = 0;
-        for (int j = 0; j < n; j++) {
-            sumaFila += m[i][j];
-            sumaColumna += m[j][i];
-        }
-        if (sumaFila != numeroMagico || sumaColumna != numeroMagico) return false;
-    }
     sumaDiagonal1 = sumaDiagonal2 = 0;
     for (int i = 0; i < n; i++) {
         sumaDiagonal1 += m[i][i];
@@ -41,57 +26,62 @@ bool esMagico() {
     return true;
 }
 
+bool esDiagonal1(int i, int j){
+    return i == j;
+}
+bool esDiagonal2(int i, int j){
+    return j == n - i - 1;
+}
 
-bool construirCuadrado(int i, int j, vector<int> sumFil, vector<int> sumCol){
-    if(i == n){
+bool construirCuadrado(int i, int j, vector<int> &sumFil, vector<int> &sumCol){
+    if(i == n) {
         if(esMagico()){
             cont++;
             if(cont == k) return true;
         }
     } else {
         if(m[i][j] == 0){
-            for(int x = 0; x < n*n; x++){
-                if(usados[x+1] == 0){
-                    m[i][j] = x + 1;
-                    usados[x + 1] = 1;
-                    sumFil[i] += x + 1;
-                    sumCol[j] += x + 1;
-                    if(j + 1 < n){
-                        if(sumFil[i] < numeroMagico && !(sumCol[j] > numeroMagico) && construirCuadrado(i, j + 1, sumFil, sumCol))
-                            return true;
-                        else{
-                            m[i][j] = 0;
-                            sumFil[i] -= x + 1;
-                            sumCol[j] -= x + 1;
-                            usados[x + 1] = 0;
-
-                        }
+            for(int x = 1; x <= n*n; x++){
+                if(usados[x] == 0){
+                    m[i][j] = x;
+                    usados[x] = 1;
+                    sumFil[i] += x;
+                    sumCol[j] += x;
+                    bool cheq_diag1 = esDiagonal1(i,j);
+                    bool cheq_diag2 = esDiagonal2(i,j);
+                    if(cheq_diag1) sumaDiagonal1 += x;
+                    if(cheq_diag2) sumaDiagonal2 += x;
+                    bool diag_ok = sumaDiagonal1 <= numeroMagico && sumaDiagonal2 <= numeroMagico;
+                    if(!(i == n - 1 && sumCol[j] != numeroMagico))
+                    {if(j + 1 < n){
+                        if(sumFil[i] < numeroMagico && diag_ok && sumCol[j] <= numeroMagico && construirCuadrado(i, j + 1, sumFil, sumCol)) return true;
                     } else {
-                        if(sumFil[i] == numeroMagico && !(sumCol[j] > numeroMagico) && construirCuadrado(i + 1, 0, sumFil, sumCol))
-                            return true;
-                        else{
-                            m[i][j] = 0;
-                            sumFil[i] -= x + 1;
-                            sumCol[j] -= x + 1;
-                            usados[x + 1] = 0;
-                        }
-                    }
+                        if(sumFil[i] == numeroMagico && diag_ok && sumCol[j] <= numeroMagico && construirCuadrado(i + 1, 0, sumFil, sumCol)) return true;
+                    }}
+                    m[i][j] = 0;
+                    sumFil[i] -= x;
+                    sumCol[j] -= x;
+                    usados[x] = 0;
+                    if(cheq_diag1) sumaDiagonal1 -= x;
+                    if(cheq_diag2) sumaDiagonal2 -= x;
                 }
             }
         }
-    } return false;
+    }
+    return false;
 }
 
 
 int main(){
     cin >> n;
     cin >> k;
-    numeroMagico = ((pow(n,3)+ n)/2);
+    numeroMagico = ((n*n*n + n)/2);
     m = vector<vector<int>>(n, vector<int>(n, 0));
     usados = vector<int>(n*n, 0);
     fil = vector<int>(n, 0);
     col = vector<int>(n, 0);
-
+    sumaDiagonal1 = 0;
+    sumaDiagonal2 = 0;
     if (construirCuadrado(0, 0, fil, col)) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {

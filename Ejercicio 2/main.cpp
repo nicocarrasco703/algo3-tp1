2431ee17afd2a667ec35b1b30e1559f79db467f8
nicolascarrasco <ncarrasco@dc.uba.ar>
2023-04-19 18:19:30 -0300

    Falta terminar

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int n, m, r;
 vector<int> v;
-unordered_map<int, bool> dp; // Tabla de programación dinámica
+vector<vector<int> > dp;
 
 int power(int x, int y) {
     unsigned long int temp;
@@ -22,27 +22,22 @@ int power(int x, int y) {
 }
 
 bool busqueda(int i, int acum) {
-    int key = i * 10000 + acum; // Crear una clave única basada en 'i' y 'acum'
-
-    // Si el valor ya se encuentra en la tabla de programación dinámica, lo devolvemos
-    if (dp.find(key) != dp.end()) {
-        return dp[key];
+    if (dp[i][acum] != -1) { // Si ya hemos calculado el resultado
+        return dp[i][acum];
     }
 
-    if (i == n) { // Si llegamos al final del vector
+    if (i == n ) { // Si llegamos al final del vector
         // Comprobamos si el acumulador es igual al resto deseado
         bool res = (acum % m) == r;
-        dp[key] = res; // Guardamos el resultado en la tabla de programación dinámica
+        dp[i][acum] = res;
         return res;
     }
-
     long long int multi = (acum * v[i]) % m;
-    // Hacemos las llamadas recursivas y actualizamos el valor de la tabla de programación dinámica
     bool res = busqueda(i + 1, (acum + v[i]) % m) ||
                busqueda(i + 1, multi) ||
                busqueda(i + 1, ((acum - v[i]) + m) % m) || // Agregamos la recursión con la resta
                busqueda(i + 1, power(acum, v[i]));
-    dp[key] = res; // Guardamos el resultado en la tabla de programación dinámica
+    dp[i][acum] = res;
     return res;
 }
 
@@ -52,12 +47,13 @@ int main() {
     while (c--) {
         cin >> n >> r >> m; // n = tamaño del vector, r = resto, m = módulo
         v.resize(n); // Redefinimos el vector con tamaño n
+        //dp.resize(n, vector<int> m); // Redefinimos la matriz con tamaño n,
+        dp = vector<vector<int> >(n + 1, vector<int>(m + 1, -1)); // Inicializamos la matriz
         for (int i = 0; i < n; i++) { // Inicializamos el vector
             int x;
             cin >> x;
             v[i] = x;
         }
-        dp.clear(); // Limpiamos la tabla de programación dinámica
         if (busqueda(0, 0)) {
             cout << "Si" << endl;
         } else {

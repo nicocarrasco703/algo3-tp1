1fa816475c3dd64c561c092279f5fb4394244fd3
tmoraut07 <tobiasmoraut7@gmail.com>
2023-05-22 16:37:45 -0300

    ej 2 terminado!!

--- TP2/Ejercicio 2/main.cpp
+++ TP2/Ejercicio 2/main.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
-int n, m;
+
+ll n, m;
 vector<vector<int>> grafo, grafo_rev;
 vector<bool> visitados;
 vector<int> pila, component;
-vector<vector<int>> grafo_condensado;
+set<int> res;
+vector<bool> tiene_entrada, condensado_tiene_entrada;
+vector<bool> tiradas;
+vector<int> vertices_raiz;
+
+
 
 void dfs1(int v) {
     visitados[v] = true;
@@ -18,7 +25,7 @@ void dfs1(int v) {
     pila.push_back(v);
 }
 
-void dfs2(int v) {
+void dfs2(int v){
     visitados[v] = true;
     component.push_back(v);
     for(auto u : grafo_rev[v]){
@@ -29,9 +36,8 @@ void dfs2(int v) {
 }
 
 void condensar(){
-    vector<int> raices(n+1, 0);
-    vector<int> vertices_raiz;
-    //vector<vector<int>> grafo_condensado(n+1);
+    vector<int> raices(n + 1, 0);
+    vector<vector<int>> grafo_condensado(n + 1);    
 
     for(auto v : pila){
         if (!visitados[v]) {
@@ -54,14 +60,15 @@ void condensar(){
             int raiz_u = raices[u];
             if (raiz_v != raiz_u){
                 grafo_condensado[raiz_v].push_back(raiz_u);
+                condensado_tiene_entrada[raiz_u] = true;
             }
         }
 }
 
 void kosaraju() {
-    visitados.assign(n + 1, false);
+   visitados = vector<bool>(n + 1, false);
    for (int i = 1; i <= n; i++){
-        if (!visitados[i])
+        if (!visitados[i] && !tiradas[i])
             dfs1(i);
     }
 
@@ -71,20 +78,50 @@ void kosaraju() {
     condensar();
 }
 
+
+void tirar(int u){
+    tiradas[u] = true;
+    for(int v : grafo[u]){
+        if(!tiradas[v]){
+            tirar(v);
+        }
+    }
+}
+
+
 int main() {
     cin >> n >> m;
     int a, b;
     grafo.assign(n + 1, vector<int>());
     grafo_rev.assign(n + 1, vector<int>());
-    grafo_condensado.assign(n + 1, vector<int>());
+
+    tiene_entrada.assign(n + 1, false);
+    condensado_tiene_entrada.assign(n + 1, false);
+    
+    tiradas.assign(n + 1, false);
+
     // inicializar grafo
     while(m--) {
         cin >> a >> b;
         grafo[a].push_back(b);
         grafo_rev[b].push_back(a);
     }
-    
+
+
+
     kosaraju();
+
+    for(int u : vertices_raiz){
+        if(!condensado_tiene_entrada[u]){
+            res.insert(u);
+            tirar(u);
+        }
+    }
+
+    cout << res.size()<< endl;
+    for(int i : res){
+        cout << i << " ";
+    }
     
     return 0;
 }

4fea207f73dc9780b01b9bdbb2b40bbbdeb4b206
nicocarrasco <ncarrasco@dc.uba.ar>
2023-05-24 12:55:20 -0300

    inicio ej3tp2

--- TP2/Ejercicio 3/main.cpp
+++ TP2/Ejercicio 3/main.cpp
@@ -1,8 +1,40 @@
+// Modems
 #include <bits/stdc++.h>
 
 using namespace std;
 
+vector<tuple<int, int>> plano;
+int n, r, w, u, v;
+vector<tuple <double, int, int, int> > E;
+int componentes;
+double Pu, Pv; 
 
-int main(){
-    return 0;
-}
+vector<tu
+
+int main() {
+    int casos;
+    int test;
+    cin >> casos;
+    for(int i = 0; i < casos; i++){
+        test ++;
+        Pu = 0;
+        Pv = 0;
+        cin >> n >> r >> w >> u >> v;
+        componentes = n;
+        plano.clear();
+        for(int i = 0; i < n; i++){
+            int x, y;
+            cin >> x >> y;
+            plano.push_back({x, y});
+        }
+        E = init_grafo(plano);
+        kruskal();
+    }
+
+
+//printTripla(E);
+//cout<<componentes<<endl;
+
+//printTripla(crear_grafo(loc));
+return 0;
+} 

3051d9d97d38d22ef90f166f847d6ca0edc6cf8c
nicocarrasco703 <ncarrasco@dc.uba.ar>
2023-06-05 13:37:40 -0300

    Ej 1 TP 3 casi terminado

--- TP3/Ejercicio 1/main.cpp
+++ TP3/Ejercicio 1/main.cpp
@@ -2,9 +2,104 @@
 
 using namespace std;
 
+typedef long long ll;
+const ll inf = 1e18;
 
+int n, m, s, t, k;
+vector<ll> salida;
+
+
+struct arista{
+    ll costo;
+    int from, to;
+    
+    bool operator<(const arista& otro) const {
+        return costo < otro.costo;
+    }
+};
+
+vector<ll> dijkstra(vector<vector<arista>> &grafo, int& src){
+    //
+    priority_queue<arista, vector<arista>, less<arista>> pq;
+    vector<ll> dist(n+1, inf);
+
+    pq.push({0, -1, src});
+    dist[src] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().to;
+        pq.pop();
+        for (arista i : grafo[u]) {
+            int v = i.to;
+            ll peso = i.costo;
+            if (dist[v] > dist[u] + peso) {
+                dist[v] = dist[u] + peso;
+                pq.push({dist[v], u, v});
+            }
+        }
+    }
+    return dist;
+}
+
+void seleccionarCandidata(vector<vector<arista>>& g, vector<vector<arista>>& g_inverso, vector<arista>& cand){
+    vector<ll> distS = dijkstra(g, s);
+    vector<ll> distT = dijkstra(g_inverso, t);
+    ll min = distS[t];
+    for(arista c : cand){
+        int u = c.from;
+        int v = c.to;
+        ll nuevoMin = distS[u] + c.costo + distT[v];
+        if (nuevoMin < min){
+            min = nuevoMin;
+        }
+    }
+    salida.push_back(min);
+}
+
+void imprimirSalida(){
+    for(ll x : salida){
+        cout << x << endl;
+    }
+}
 
 
 int main() {
+    int cantCasos;
+    cin >> cantCasos;
+    vector<vector<arista>> grafo;
+    vector<vector<arista>> grafo_inverso;
+    vector<arista> candidatas;
+    while(cantCasos--){
+        cin >> n >> m >> k >> s >> t;
+        grafo.clear();
+        grafo_inverso.clear();
+        candidatas.clear();
+        grafo.resize(n+1);
+        grafo_inverso.resize(n+1);
+
+        // inicializar grafos
+        for(int i = 0; i < m; i++){
+            int x, y;
+            ll c;
+            cin >> x >> y >> c;
+            arista a = {c, x, y};
+            grafo[x].push_back(a);
+            grafo_inverso[y].push_back(a);
+        }
+        // inicializar calles candidatas
+        for (int i = 0; i < k; i++)
+        {
+            int x, y;
+            ll c;
+            cin >> x >> y >> c;
+            arista a = {c, x, y};
+            candidatas.push_back(a);
+        }
+        // resolver
+        seleccionarCandidata(grafo, grafo_inverso, candidatas);
+    }
+
+    imprimirSalida();
+
     return 0;
 } 

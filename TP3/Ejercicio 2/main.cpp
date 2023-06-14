a0c941b8318e1ff248b8c0388d24acaec55b6ff8
tmoraut07 <tobiasmoraut7@gmail.com>
2023-06-14 19:11:57 -0300

    ¬

--- TP3/Ejercicio 2/main.cpp
+++ TP3/Ejercicio 2/main.cpp
@@ -50,39 +50,41 @@ int maxflujo(int s, int t) {
     return flujo;
 }
 
-
+void escanear(){
+    cin >> n >> m;
+    if(n == 0 & m == 0){
+        return;
+    }
+    capacidad.assign(n+2, vector<int>(n+2, 0));
+    adj.assign(n+2, vector<int>());
+    for (int i = 1; i < n + 1; i++){
+        int x;
+        cin >> x;
+        if(x == 0){
+            adj[0].push_back(i);
+            capacidad[0][i] = 1;
+        } else {
+            adj[n + 1].push_back(i);
+            capacidad[n + 1][i] = 1;
+        }
+    }
+    for (int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        capacidad[u][v] = 1;
+        capacidad[v][u] = 1;
+    }
+    int res = maxflujo(0, n+1);
+    cout << res << "\n"; 
+    escanear();
+}
 
 int main() {
     // IO rapido
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int cantCasos;
-    cin >> cantCasos;
-    while (cantCasos--){
-        cin >> n >> m;
-        capacidad.assign(n+2, vector<int>(n+2, 0));
-        adj.assign(n+2, vector<int>());
-        for (int i = 1; i < n + 1; i++){
-            int x;
-            cin >> x;
-            if(x == 0){
-                adj[0].push_back(i);
-                capacidad[0][i] = 1;
-            } else {
-                adj[n + 1].push_back(i);
-                capacidad[n + 1][i] = 1;
-            }
-        }
-        for (int i = 0; i < m; i++){
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            capacidad[u][v] = 1;
-            capacidad[v][u] = 1;
-        }
-        int res = maxflujo(0, n+1);
-        cout << res << "\n";
-    }
+    escanear();
     return 0;
 } 
